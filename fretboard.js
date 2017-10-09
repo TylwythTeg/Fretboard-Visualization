2a3dd42214dd513c766e953d35aba5fd331afc6c
TylwythTeg <garciarobertoiv@yahoo.com>
2017-10-09 00:41:37 -0700

    I am now generating the guitar strings and notes via javascript. Right now it is Build all strings and then Build all notes on each string, and I'm not sure if that is the best way. Also now accepts a tuning with a variable amount of strings (6-7) and will resize the fretboard accordingly. I'm sure this code could be refactored heavily

--- fretboard.js
+++ fretboard.js
@@ -134,87 +134,101 @@ var fretboard = {
     ]
 };
 
-/* We can just reverse the small array */
-console.log(fretboard.strings.reverse());
 
 
 
-
-
-// C
-console.log(fretboard.strings[4].notes[1]);
-
-var strings = document.querySelectorAll(".strings");
-console.log(strings);
-strings = strings[0];
-console.log(strings);
-
-//strings = strings.children;
-//strings[0].className = "jump.hey";
+/*      We can just reverse a copy of the strings.          */
+/*      Note that the array's elements are still the same   */
+/*      as they point to the same object references         */
+strings = fretboard.strings.slice().reverse();
 console.log(strings);
 
 
-console.log(fretboard.strings.length);
-/* We need to iterate from end to start because DOM order matters */
-last = fretboard.strings.length - 1;
-for (var i = last; i >= 0; i--) {
-    /* add each string */
-    var element = document.createElement("li");
-    strings.appendChild(element);
-    
-    /* notate the open string? */
-    element.setAttribute("data-note",fretboard.strings[i].openNote);
+// expect "div"
+// eg addChild(stringsContainer, "li", {"data-note": "Z"});
+function addChild(parent, child, attributes) {
+    child = document.createElement(child);
+    if (attributes) {
+        for (var attr in attributes) {
+            child.setAttribute(attr, attributes[attr])
+        }
+    }
+    parent.appendChild(child);
+    return child;
 }
 
-/* Iterate through the string lists */
-var strings = document.querySelectorAll(".strings");
-strings = strings[0];
 
-var i = (fretboard.strings.length - 1) + 6;
-for (child in strings.children) {
-    console.log("i below");
-    console.log(i);
-    var string = strings.children[child];
-    console.log(string);
-    console.log("test");
-    if (fretboard.strings[i] === undefined) {
-            i--;
-            continue;
+/* Right now we are only set up for one fretboard per page      */
+/* I definitely would not want to be limited to one ultimately  */
+/* But this is a good place to start  */
+
+/* Get Unordered List Element */
+var stringsContainer= document.getElementById("strings");
+console.dir(stringsContainer);
+
+/* generates String list items */
+function generateStrings () {
+    var stringsContainer = document.getElementById("strings");
+    
+    for (var i = 0; i < strings.length; i++) {
+        var string = strings[i];
+        addChild(stringsContainer, "li", {"data-note": string.openNote})
+    }
+}
+generateStrings();
+
+/* Generate note container */
+function generateNoteContainers () {
+    var stringElements = stringsContainer.children;
+    for (var i = 0; i < stringElements.length; i++) {
+        var string = stringElements[i];
+        // ".clear_div" firefox margin workaround
+        addChild(string, "div", {"class": "clear_div"});
+        addChild(string, "ul", {"class": "notes"});
+    }  
+}
+
+generateNoteContainers();
+
+/* The fun part: Generating notes on each string */
+function note_generators() {
+    // Closure that uses i from the for loop
+    function generateNotes(container) {
+        var string = strings[i];
+        for (var j = 0; j < string.notes.length; j++) {
+            var child = addChild(container, "li");
+            child.textContent = string.notes[j];
+        }
+        
     }
     
-    if (i < 0) {break;}
-    if (string.nodeType !== 1) {continue;}
+    // note if we scale to multiple fretboards this will need to change
+    var noteContainers = document.getElementsByClassName("notes");
     
-    console.log(fretboard.strings[i]);
-    
-        console.log("made it this far");
-        console.log("note" + i);
-        console.log(fretboard.strings[i].openNote);
-        
-        console.log(fretboard.strings[i].openNote + "" + string.getAttribute("data-note"));
-        if (string.getAttribute("data-note") === fretboard.strings[i].openNote){
-            /* temp check to make sure this one we add to */
-            console.log((fretboard.strings.length - 1) + 7);
-            console.log("testtestetsetst");
-            if (i === (fretboard.strings.length - 1) + 7) {
-                /* if the first go round the loop then add the clear div */
-                console.log("what about this this this");
-                var element = document.createElement("div");
-                element.className = "clear_div";
-                string.appendChild(element);
-                
-                var element = document.createElement("ul");
-                element.className = "notes";
-                string.appendChild(element);
-            }
-            
-            /* otherwise just add the notes */
-            var element = document.createElement("ul");
-            element.className = "notes";
-            string.appendChild(element);
-
-        }
-    i--;
+    for (var i = 0; i < noteContainers.length; i++ ) {
+        generateNotes(noteContainers[i]);
+    }
 }
+
+note_generators();
+
+
+/* update heights in CSS of elements according to num of strings */
+function updateHeights() {
+    
+    var pixels = {
+        "4": "166px",
+        "5": "206px",
+        "6": "246px",
+        "7": "286px",
+    };
+    
+    var num = strings.length;
+    
+    var neck = document.getElementsByClassName("neck");
+    neck[0].style.height = pixels[num];
+}
+
+updateHeights();
 
 
